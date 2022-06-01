38bf364aa6e989113e933da33fee3abad976cee2
Yaroslav Yakimov <yayak0072@gmail.com>
2022-06-01 09:25:07 +0300

    Version 2: service improvement

--- server_setup.py
+++ server_setup.py
@@ -1,5 +1,19 @@
-file = open("/etc/rc.local", "w")
+file = open("/etc/systemd/system/mh.service", "w")
 
-file.write("bash /home/ubuntu/mh/server_setup.sh")
+file.write('''
+
+[Unit]
+
+Description=mh
+
+[Service]
+
+ExecStart=/usr/local/bin/mh/server_setup.sh
+
+[Install]
+
+WantedBy=multi-user.target
+
+''')
 
 file.close();
--- server_setup.sh
+++ server_setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
